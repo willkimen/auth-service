03298b4de2af45718cb2fa1f73ef74206f6394f2
lab <lab@example.com>
2026-07-26 15:57:22 -0300

    feat: add the endpoint that revoke all refreshes tokens

--- src/adapters/inputs/api/endpoints/auth/revoke_all_refreshes.py
+++ src/adapters/inputs/api/endpoints/auth/revoke_all_refreshes.py
@@ -1,12 +1,14 @@
-from fastapi import status
+from typing import Annotated
+
+from fastapi import Depends, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 from adapters.inputs.api.dependencies.use_cases import (
     RevokeAllRefreshesDep,
 )
 from adapters.inputs.api.routers import auth_router
-from adapters.inputs.api.schemas import (
-    RefreshBodyRequest,
-)
+
+bearer_scheme = HTTPBearer()
 
 
 @auth_router.post(
@@ -14,7 +16,9 @@ from adapters.inputs.api.schemas import (
     status_code=status.HTTP_204_NO_CONTENT,
 )
 async def revoke_all_refreshes(
-    body: RefreshBodyRequest,
+    header_authorization: Annotated[
+        HTTPAuthorizationCredentials, Depends(bearer_scheme)
+    ],
     use_case: RevokeAllRefreshesDep,
 ):
     """
@@ -22,9 +26,13 @@ async def revoke_all_refreshes(
     invalidating all active refresh-token sessions.
 
     Args:
-        `body` (`RefreshBodyRequest`):
-            - Request body containing the refresh token used to identify
-              the authenticated user whose refresh tokens should be revoked.
+        `header_authorization` (`HTTPAuthorizationCredentials`):
+            - Authorization header containing the access token used to
+              identify the authenticated user whose refresh tokens should
+              be revoked.
+        `use_case` (`RevokeAllRefreshesDep`):
+            - Injected application use case responsible for revoking all
+              refresh tokens associated with the authenticated user.
 
     Raises:
         `InfrastructureError`:
@@ -34,4 +42,4 @@ async def revoke_all_refreshes(
         `InvalidTokenTypeError`:
             - If token type is not a refresh token.
     """
-    await use_case.execute(body.refresh)
+    await use_case.execute(header_authorization.credentials)
---
+++ tests/integration/adapters/inputs/api/endpoints/auth/test_revoke_all_refreshes.py
@@ -0,0 +1,132 @@
+from httpx2 import AsyncClient
+
+from adapters.inputs.api.dependencies.use_cases import (
+    revoke_all_refreshes_factory,
+)
+from application.exceptions import (
+    CorruptedPersistenceStateError,
+    EmailAlreadyUsedError,
+)
+from domain.exceptions import InactiveUserError, UserErrorCode
+
+headers_dummy = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Bearer jwtdummy',
+}
+
+
+async def test_return_correctly_response_data(
+    async_client: AsyncClient,
+    clean_database: None,
+    get_settings_override: None,
+    create_access_token: str,
+    create_refresh_token: str,
+    persist_valid_refresh_token: None,
+):
+    # arrange
+    expected_status_code = 204
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {create_access_token}',
+    }
+
+    # act
+    response = await async_client.post(
+        '/api/v1/auth/token/revoke-all-refreshes',
+        headers=headers,
+    )
+
+    # asserts
+    assert response.status_code == expected_status_code
+
+
+async def test_should_handle_unexpected_exception(
+    async_client: AsyncClient,
+    use_case_override_with_error,
+):
+    # arrange
+    use_case_override_with_error(revoke_all_refreshes_factory, Exception())
+    expected_status_code = 500
+
+    # act
+    actual_response = await async_client.post(
+        '/api/v1/auth/token/revoke-all-refreshes',
+        headers=headers_dummy,
+    )
+
+    # asserts
+    assert actual_response.status_code == expected_status_code
+    response_data = actual_response.json()
+    assert response_data['error'] == 'internal error server'
+
+
+async def test_should_handle_domain_exception(
+    async_client: AsyncClient,
+    use_case_override_with_error,
+):
+    # arrange
+    use_case_override_with_error(
+        revoke_all_refreshes_factory, InactiveUserError()
+    )
+    expected_status_code = 403
+
+    # act
+    actual_response = await async_client.post(
+        '/api/v1/auth/token/revoke-all-refreshes',
+        headers=headers_dummy,
+    )
+
+    # asserts
+    assert actual_response.status_code == expected_status_code
+    response_data = actual_response.json()['error']
+    assert response_data['code'] == UserErrorCode.INACTIVE_USER
+    assert response_data['message'] == 'User account is inactive'
+
+
+async def test_should_handle_corrupted_persistence_state_exception(
+    async_client: AsyncClient,
+    use_case_override_with_error,
+):
+    # arrange
+    use_case_override_with_error(
+        revoke_all_refreshes_factory, CorruptedPersistenceStateError()
+    )
+    expected_status_code = 500
+
+    # act
+    actual_response = await async_client.post(
+        '/api/v1/auth/token/revoke-all-refreshes',
+        headers=headers_dummy,
+    )
+
+    # asserts
+    assert actual_response.status_code == expected_status_code
+    response_data = actual_response.json()
+    assert response_data['error'] == 'internal error server'
+
+
+async def test_should_handle_application_exception(
+    async_client: AsyncClient,
+    use_case_override_with_error,
+):
+    # arrange
+    use_case_override_with_error(
+        revoke_all_refreshes_factory, EmailAlreadyUsedError()
+    )
+    expected_status_code = 409
+
+    # act
+    actual_response = await async_client.post(
+        '/api/v1/auth/token/revoke-all-refreshes',
+        headers=headers_dummy,
+    )
+
+    # asserts
+    assert actual_response.status_code == expected_status_code
+    response_data = actual_response.json()['error']
+    assert response_data['code'] == 'EMAIL_ALREADY_USE'
+    assert response_data['message'] == (
+        'An account with this email already exists'
+    )
